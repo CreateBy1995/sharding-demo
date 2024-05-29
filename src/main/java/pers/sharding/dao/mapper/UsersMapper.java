0eb9cf11e0f756238bf53da87ad15599d37e792b
CreateBy1995 <592329774@qq.com>
2024-05-29 23:40:24 +0800

    update

---
+++ src/main/java/pers/sharding/dao/mapper/UsersMapper.java
@@ -0,0 +1,15 @@
+package pers.sharding.dao.mapper;
+
+import org.apache.catalina.User;
+import org.apache.ibatis.annotations.Param;
+import pers.sharding.dao.domain.Order;
+import pers.sharding.dao.domain.Users;
+
+import java.util.List;
+
+public interface UsersMapper {
+
+    List<Users> listByIds(@Param("ids") List<Long> id);
+
+//    int create(Order order);
+}
---
+++ src/main/java/pers/sharding/service/UsersService.java
@@ -0,0 +1,22 @@
+package pers.sharding.service;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pers.sharding.dao.domain.Order;
+import pers.sharding.dao.domain.Users;
+import pers.sharding.dao.mapper.OrderMapper;
+import pers.sharding.dao.mapper.UsersMapper;
+import pers.sharding.ro.OrderCreateRO;
+
+import java.util.List;
+
+@Service
+public class UsersService {
+    @Autowired
+    private UsersMapper usersMapper;
+    public List<Users> listByIds(List<Long> ids){
+        return usersMapper.listByIds(ids);
+    }
+
+}
