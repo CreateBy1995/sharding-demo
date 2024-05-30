633d1fd20a5858c350ac66730700d655ad461c69
dongcongxu <congxu.dong@ubtrobot.com>
2024-05-30 09:56:31 +0800

    update

--- src/main/java/pers/sharding/util/ReflectionUtil.java
+++ src/main/java/pers/sharding/util/ReflectionUtil.java
@@ -1,8 +1,12 @@
 package pers.sharding.util;
 
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class ReflectionUtil {
@@ -18,5 +22,22 @@ public class ReflectionUtil {
             log.error("convert failed, targetClass:{}, source:{}", targetClass, source);
         }
         return targetInstance;
+    }
+
+    public static <T> List<T> convertList(List<Object> source, Class<T> targetClass) {
+        Assert.notNull(source, "source can't be null ");
+        Assert.notNull(targetClass, "targetClass can't be null ");
+        List<T> targetList = new ArrayList<>();
+        for (int i = 0; i < source.size(); i++) {
+            T targetInstance = null;
+            try {
+                targetInstance = targetClass.newInstance();
+                BeanUtils.copyProperties(source, targetInstance);
+                targetList.add(targetInstance);
+            } catch (InstantiationException | IllegalAccessException e) {
+                log.error("convert failed, targetClass:{}, source:{}", targetClass, source);
+            }
+        }
+        return targetList;
     }
 }
