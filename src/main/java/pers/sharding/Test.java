ce1f15ee89453b24a3f9c2bd353cfcc580daed76
CreateBy1995 <592329774@qq.com>
2024-06-12 22:09:23 +0800

    update

--- src/main/java/pers/sharding/Test.java
+++ src/main/java/pers/sharding/Test.java
@@ -6,15 +6,5 @@ package pers.sharding;
  * @Description:
  */
 public class Test {
-    public static void main(String[] args) {
-//        new GroovyShell()
-//        String str = "tb_${(id >> 1) % 4}";
-//        Closure<?> result = new InlineExpressionParser(str).evaluateClosure().rehydrate(new Expando(), null, null);
-//        result.setResolveStrategy(Closure.DELEGATE_ONLY);
-//        result.setProperty("id", 1L);
-//        System.out.println(result.call().toString());
-//
-//        {it -> "tb_${(id >> 1) % 4}"}
-    }
 
 }
