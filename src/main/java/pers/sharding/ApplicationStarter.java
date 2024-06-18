4063b9595554767ef9e9526fd7ede13de9e1e643
dongcongxu <congxu.dong@ubtrobot.com>
2024-06-18 18:18:24 +0800

    广播表

--- src/main/java/pers/sharding/ApplicationStarter.java
+++ src/main/java/pers/sharding/ApplicationStarter.java
@@ -11,11 +11,12 @@ public class ApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class);
 //        for (int i = 0; i < 20; i++) {
-//            System.out.println(String.format("i = %d, mod = %d", i , i >> 1 % 4));
-//            Closure<?> result = new InlineExpressionParser("users_${(id >> 1) % 4}").evaluateClosure().rehydrate(new Expando(), null, null);
-//            result.setResolveStrategy(Closure.DELEGATE_ONLY);
-//            result.setProperty("id", i);
-//            System.out.println(String.format("i = %d ,script = %s", i, result.call().toString()));
+////            System.out.println(String.format("i = %d, mod = %d", i , ));
+////            Closure<?> result = new InlineExpressionParser("users_${(id >> 1) % 4}").evaluateClosure().rehydrate(new Expando(), null, null);
+////            result.setResolveStrategy(Closure.DELEGATE_ONLY);
+////            result.setProperty("id", i);
+////            System.out.println(String.format("i = %d ,script = %s", i, result.call().toString()));
+//            System.out.println(String.format("ds = %d ,tb = %d", i % 2, i%4));
 //        }
 
 
