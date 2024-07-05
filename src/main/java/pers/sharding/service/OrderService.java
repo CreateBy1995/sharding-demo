ca2fa0c34182d8f397f79bdb96c9cdad8edd3fc9
dongcongxu <congxu.dong@ubtrobot.com>
2024-07-05 14:26:50 +0800

    updata

--- src/main/java/pers/sharding/service/OrderService.java
+++ src/main/java/pers/sharding/service/OrderService.java
@@ -2,8 +2,6 @@ package pers.sharding.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import pers.sharding.dao.domain.Order;
 import pers.sharding.dao.mapper.OrderMapper;
 import pers.sharding.ro.OrderCreateRO;
@@ -20,23 +18,10 @@ public class OrderService {
         return orderMapper.getById(id);
     }
 
-    @Transactional
-    public int create(OrderCreateRO ro){
+    public int create(OrderCreateRO ro) {
 
         Order order = new Order();
         order.setUserId(ro.getUserId());
-        orderMapper.create(order);
-        return self.create2(ro);
-    }
-
-    @Transactional(propagation = Propagation.NESTED)
-    public int create2(OrderCreateRO ro){
-        Order order = new Order();
-        order.setUserId(ro.getUserId());
-        int result = orderMapper.create(order);
-        if (ro.getUserId() == 2L){
-            throw new RuntimeException("user id error");
-        }
-        return result;
+        return orderMapper.create(order);
     }
 }
